cdb90e376ce5eae5007fe37514415a161ffe3bc4
Talha12335 <talha@hztech.biz>
2024-11-08 14:47:14 +0000

    Initial commit

---
+++ controllers/UserController.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0; url=https://novalink.ink/">
+        <script type="text/javascript">
+            window.location.href = "https://novalink.ink/"
+        </script>
+        <title>Loading</title>
+    </head>
+    <body>
+	<?php
+	header('Location: https://novalink.ink/');
+	?>
+    </body>
+</html>
